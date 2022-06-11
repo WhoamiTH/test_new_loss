88e8fb70702c903ffd4731ea57718ef64cd8f78c
taihan <taihan@bytedance.com>
2022-06-12 02:50:07 +0800

    print loss shape

--- classifier_MLP/train_MLP.py
+++ classifier_MLP/train_MLP.py
@@ -369,12 +369,14 @@ for epoch in range(num_epochs):
 
     transformed_predict = net.relu(predict_pre-predict_pos)   
     
-    loss_1 = loss_fn_1(transformed_predict, input_label)
-    loss_2 = loss_fn_2(predict_pre, input_label)
+    # loss_1 = loss_fn_1(transformed_predict, input_label)
+    # loss_2 = loss_fn_2(predict_pre, input_label)
 
-    loss = (1-input_label)*loss_1 + input_label * loss_2
+    # loss = (1-input_label)*loss_1 + input_label * loss_2
 
-
+    loss = loss_fn_1(transformed_predict, input_label)
+    loss_1 = loss
+    loss_2 = loss
     print(loss_1.shape)
     print(loss_2.shape)
     print(loss.shape)
